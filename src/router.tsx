b5371fce51e67b5d742f2652ff2cb72f814c8549
hyeona01 <sha0209@dgu.ac.kr>
2025-01-11 11:59:25 +0000

    ✨ feat: 색상 변수 추가하기 #3

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,25 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Outlet, Route, Routes } from 'react-router-dom';
 import HomePage from '@/pages/HomePage';
 import StartPage from '@/pages/StartPage';
+import ScrollToTop from '@/components/Common/ScrollToTop';
+
+const Layout = () => {
+  return (
+    <>
+      <ScrollToTop />
+      <Outlet />
+    </>
+  );
+};
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/start" element={<StartPage />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/start" element={<StartPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
